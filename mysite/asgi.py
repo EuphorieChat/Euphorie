58109f653c8e2213cbd8a577be2552cad1371e37
Stephen Lee <jungmindl@gmail.com>
2025-05-04 18:32:30 +0900

    routing.py updated

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -1,27 +1,12 @@
 import os
-import logging
-
-# Set up logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-# 👇 MUST be set before anything else
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
-logger.info("Setting up ASGI application...")
-
-# 👇 Setup Django *before* importing anything Django-related
 import django
-django.setup()
-
-logger.info("Django setup complete, importing routing...")
-
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 import chat.routing
 
-logger.info("Creating ProtocolTypeRouter...")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
@@ -31,5 +16,3 @@ application = ProtocolTypeRouter({
         )
     ),
 })
-
-logger.info("ASGI application initialized successfully!")
