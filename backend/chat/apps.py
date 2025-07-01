5ff113d38e21ea802a88d62a2323d115cf8cb4d2
EuphorieChat <your-email@example.com>
2025-07-01 14:06:41 +0900

    settings.py update

--- backend/chat/apps.py
+++ backend/chat/apps.py
@@ -1,9 +1,5 @@
-# backend/chat/apps.py
-
 from django.apps import AppConfig
 
-class ChatConfig(AppConfig):
+class RoomsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'rooms'
-    
-    # No ready() method for now to avoid import issues
