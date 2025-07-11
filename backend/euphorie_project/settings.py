6c3db2c882fef5c8e020d97299e9a45d3801dcbe
EuphorieChat <your-email@example.com>
2025-07-11 13:34:43 +0900

    settings.py update

--- backend/euphorie_project/settings.py
+++ backend/euphorie_project/settings.py
@@ -275,7 +275,7 @@ SOCIALACCOUNT_PROVIDERS = {
     'google': {
         'APP': {
             'client_id': os.getenv('GOOGLE_CLIENT_ID'),
-            'secret': os.getenv('GOOGLE_SECRET'),
+            'secret': os.getenv('GOOGLE_CLIENT_SECRET'),  
             'key': ''
         },
         'SCOPE': [
